031ddac836e8a46ca4cc03422a7b73da6ff01947
K-Schaeffer <kcb.schaeffer@gmail.com>
2020-02-19 20:46:37 -0300

    Finishing chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,51 +1,71 @@
 <template>
   <v-container>
-    <v-textarea
+    <v-text-field
       label="Mensagem"
       placeholder="Digite a sua mensagem"
-      v-model="chat"
+      v-model="mes.mensagem"
     >
-    </v-textarea>
+    </v-text-field>
 
-    <v-btn color="success" @click="enviar()"> Enviar </v-btn>
+    <v-btn color="success" @click="salvar()"> Enviar </v-btn>
+
+    <v-timeline>
+      <v-timeline-item
+        v-for="msg in chat"
+        :key="msg.codigo"
+        icon="mdi-message-reply"
+        icon-color="green"
+        color="black"
+      >
+        <template v-slot:icon> </template>
+        <span slot="opposite">{{ msg.data }}</span>
+        <v-card class="elevation-2">
+          <v-card-title class="headline">{{ msg.usuario }}</v-card-title>
+          <v-card-text>
+            {{ msg.mensagem }}
+          </v-card-text>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
   </v-container>
 </template>
 
 <script>
 import axios from "axios";
-
 export default {
   name: "Chat",
   data: () => ({
-    perfil: "Kauan",
-    chat: {
-      mensagem: "Oi",
-      usuario: "Kauan",
-      data: "20/20/20",
-      //data = new Date().toLocaleTimeString("pt-br")
-    }
+    chat: [],
+    mes: {}
   }),
   methods: {
-    enviar() {
-      let url = "http://api.anticasper.com/index.php?acao=mandarmensagem";
-      axios
-        .post(url, this.chat)
-        .then(res => {
-          this.sucesso = res;
-          console.log(this.sucesso);
-        })
-        .catch(err => alert(err));
-    },
-    listar() {
+    chatats() {
       let url = "http://api.anticasper.com/index.php?acao=chat";
       axios
         .get(url)
         .then(res => {
-          //Then se dar certo
-          this.listaUsuarios = res.data;
+          this.chat = res.data;
+        })
+        .catch(err => alert(err));
+    },
+    salvar() {
+      this.mes.usuario = "Kauan";
+      this.mes.data = new Date().toLocaleTimeString("pt-BR");
+      let url = "http://api.anticasper.com/index.php?acao=mandarmensagem";
+      axios
+        .post(url, this.mes)
+        .then(res => {
+          console.log(res);
+          this.mes = {};
+          this.chat = this.chatats();
         })
         .catch(err => alert(err));
     }
+  },
+  mounted() {
+    setInterval(() => {
+      this.chatats();
+    }, 1000);
   }
 };
 </script>
